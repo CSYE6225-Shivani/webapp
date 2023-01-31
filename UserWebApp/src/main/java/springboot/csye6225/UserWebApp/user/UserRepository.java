04cce490a990abc49dddf868346ccebc8feba705
Shivani31996 <shivani.chavan96@gmail.com>
2023-01-31 13:34:52 -0500

    Added UserController, service and repository

---
+++ UserWebApp/src/main/java/springboot/csye6225/UserWebApp/user/UserRepository.java
@@ -0,0 +1,14 @@
+package springboot.csye6225.UserWebApp.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+    @Query("SELECT u FROM user_details u WHERE u.username = ?1")
+    Optional<User> findUserByUsername(String username);
+
+}
