2c14b608800f8e8b62419ce0828a7b391b4a10e8
Shivani31996 <shivani.chavan96@gmail.com>
2023-02-07 16:14:43 -0500

    Added Product Validations

--- UserWebApp/src/main/java/springboot/csye6225/UserWebApp/product/ProductRepository.java
+++ UserWebApp/src/main/java/springboot/csye6225/UserWebApp/product/ProductRepository.java
@@ -8,4 +8,5 @@ import java.util.Optional;
 @Repository
 public interface ProductRepository extends JpaRepository<Product,Long> {
     Optional<Product> findProductById(Long id);
+    Optional<Product> findProductBySku(String sku);
 }
