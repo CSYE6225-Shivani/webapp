2896c20b1b8705c3268ee0041c7e884044004758
Shivani31996 <shivani.chavan96@gmail.com>
2023-02-26 15:00:11 -0500

    Testing RDS

--- UserWebApp/src/main/java/springboot/csye6225/UserWebApp/image/ImageController.java
+++ UserWebApp/src/main/java/springboot/csye6225/UserWebApp/image/ImageController.java
@@ -1,27 +1,32 @@
-package springboot.csye6225.UserWebApp.image;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import javax.servlet.http.HttpServletRequest;
-
-@RestController
-@RequestMapping
-@Transactional
-public class ImageController {
-    private ImageServices imageServices;
-
-    @Autowired
-    public ImageController(ImageServices imageServices) {
-        this.imageServices = imageServices;
-    }
-
-    @PostMapping(produces = "application/json",path = "v1/product/{product_id}/image")
-    public ResponseEntity<Object> uploadImage(HttpServletRequest request, @RequestPart(value = "file") MultipartFile multipartFile, @PathVariable("product_id") Long product_id) {
-        ResponseEntity<Object> result = imageServices.uploadImage(request,multipartFile,product_id);
-        return result;
-    }
-}
+//package springboot.csye6225.UserWebApp.image;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.HttpEntity;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.transaction.annotation.Transactional;
+//import org.springframework.web.bind.annotation.*;
+//import org.springframework.web.multipart.MultipartFile;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import java.io.IOException;
+//import java.io.InputStream;
+//
+//@RestController
+//@RequestMapping
+//@Transactional
+//public class ImageController {
+//    private ImageServices imageServices;
+//
+//    @Autowired
+//    public ImageController(ImageServices imageServices) {
+//        this.imageServices = imageServices;
+//    }
+//
+//    @PostMapping(path = "v1/product/{product_id}/image")
+//    public ResponseEntity<Object> uploadImage(HttpServletRequest request,MultipartFile multipartFile, @PathVariable("product_id") Long product_id) throws IOException
+//    {
+//        ResponseEntity<Object> result = imageServices.uploadImage(request,multipartFile,product_id);
+//        return new ResponseEntity<>("ok", HttpStatus.OK);
+//    }
+//}
--- UserWebApp/src/main/java/springboot/csye6225/UserWebApp/image/S3Config.java
+++ UserWebApp/src/main/java/springboot/csye6225/UserWebApp/image/S3Config.java
@@ -1,24 +1,32 @@
-package springboot.csye6225.UserWebApp.image;
-
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-
-@Configuration
-public class S3Config {
-    @Value("${AWS_REGION}")
-    private String aws_region;
-
-    @Bean
-    @Primary
-    AmazonS3 getS3Client()
-    {
-        return AmazonS3ClientBuilder
-                .standard()
-                .withRegion(aws_region)
-                .build();
-    }
-}
+//package springboot.csye6225.UserWebApp.image;
+//import com.amazonaws.auth.BasicAWSCredentials;
+//import com.amazonaws.services.s3.AmazonS3;
+//import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Primary;
+//
+//@Configuration
+//public class S3Config {
+//    @Value("${amazonProperties.region}")
+//    private String aws_region;
+//
+//    // Access key id will be read from the application.properties file during the application intialization.
+//    @Value("${amazonProperties.access_key_id}")
+//    private String accessKeyId;
+//    // Secret access key will be read from the application.properties file during the application intialization.
+//    @Value("${amazonProperties.secret_access_key}")
+//    private String secretAccessKey;
+//
+//    @Bean
+//    @Primary
+//    AmazonS3 getS3Client()
+//    {
+//        BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials(accessKeyId, secretAccessKey);
+//        return AmazonS3ClientBuilder
+//                .standard()
+//                .withRegion(aws_region)
+//                .build();
+//    }
+//}
