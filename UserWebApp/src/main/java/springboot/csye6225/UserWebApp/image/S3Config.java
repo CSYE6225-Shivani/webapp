6f2419a49350c293e17c3f2363e6e7253e54962e
Shivani31996 <shivani.chavan96@gmail.com>
2023-02-28 18:48:42 -0500

    Testing after adding bucket id

--- UserWebApp/src/main/java/springboot/csye6225/UserWebApp/image/S3Config.java
+++ UserWebApp/src/main/java/springboot/csye6225/UserWebApp/image/S3Config.java
@@ -1,49 +1,49 @@
-package springboot.csye6225.UserWebApp.image;
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.auth.InstanceProfileCredentialsProvider;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-
-@Configuration
-public class S3Config {
-
-    @Value("${aws_region.aws_region}")
-    private String awsRegion;
-
-//    public AWSCredentials credentials() {
-//        AWSCredentials credentials = new BasicAWSCredentials(
-//                "AKIA35FCJK2XQHOFBISS",
-//                "TTK0QJ6EYfLRxdkMlfR/DKtYNo1KK9Jdw/SicJs4"
-//        );
-//        return credentials;
-//    }
-
+//package springboot.csye6225.UserWebApp.image;
+//import com.amazonaws.auth.AWSCredentials;
+//import com.amazonaws.auth.AWSStaticCredentialsProvider;
+//import com.amazonaws.auth.BasicAWSCredentials;
+//import com.amazonaws.auth.InstanceProfileCredentialsProvider;
+//import com.amazonaws.services.s3.AmazonS3;
+//import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Primary;
+//
+//@Configuration
+//public class S3Config {
+//
+//    @Value("${aws_region.aws_region}")
+//    private String awsRegion;
+//
+////    public AWSCredentials credentials() {
+////        AWSCredentials credentials = new BasicAWSCredentials(
+////                "AKIA35FCJK2XQHOFBISS",
+////                "TTK0QJ6EYfLRxdkMlfR/DKtYNo1KK9Jdw/SicJs4"
+////        );
+////        return credentials;
+////    }
+//
+////    @Bean
+////    @Primary
+////    public AmazonS3 getS3Client()
+////    {
+////        AmazonS3 amazonS3= AmazonS3ClientBuilder
+////                .standard()
+////                .withCredentials(new AWSStaticCredentialsProvider(credentials()))
+////                .withRegion(awsRegion)
+////                .build();
+////        return amazonS3;
+////    }
+//
 //    @Bean
 //    @Primary
-//    public AmazonS3 getS3Client()
+//    public AmazonS3 amazonS3()
 //    {
-//        AmazonS3 amazonS3= AmazonS3ClientBuilder
+//        return AmazonS3ClientBuilder
 //                .standard()
-//                .withCredentials(new AWSStaticCredentialsProvider(credentials()))
+//                .withCredentials(new InstanceProfileCredentialsProvider(false))
 //                .withRegion(awsRegion)
 //                .build();
-//        return amazonS3;
 //    }
-
-    @Bean
-    @Primary
-    public AmazonS3 amazonS3()
-    {
-        return AmazonS3ClientBuilder
-                .standard()
-                .withCredentials(new InstanceProfileCredentialsProvider(false))
-                .withRegion(awsRegion)
-                .build();
-    }
-}
+//}
