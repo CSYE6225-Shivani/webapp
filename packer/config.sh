4493da05a1d3acc84f36d734695edf0efd8421f2
Shivani31996 <shivani.chavan96@gmail.com>
2023-02-28 20:34:41 -0500

    Testing packer build

--- packer/config.sh
+++ packer/config.sh
@@ -11,14 +11,8 @@ sudo yum clean metadata && sudo yum install java-11-openjdk -y
 echo "Java version"
 java --version
 
-### MAVEN setup ############################
-#echo "Installing Maven"
-#sudo wget https://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
-#sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
-#sudo yum install -y apache-maven
-
 ### POSTGRES setup ############################
-echo "Installing POSTGRES Client only psql"
+echo "Installing POSTGRES Client psql"
 sudo amazon-linux-extras enable postgresql14
 sudo yum clean metadata && sudo yum install postgresql -y
 
@@ -41,15 +35,15 @@ sudo yum clean metadata && sudo yum install postgresql -y
 #sudo -u postgres psql -c "CREATE USER shivanichavan WITH PASSWORD 'Chavan123';"
 #sudo -u postgres psql -c 'GRANT ALL PRIVILEGES ON DATABASE userdatabase TO shivanichavan;'
 
-##Moving the service file to systemd location
 echo "Transferring Jar File & changing permissions to read and execute"
 sudo mv /tmp/UserWebApp-0.0.1-SNAPSHOT.jar /usr/bin/UserWebApp-0.0.1-SNAPSHOT.jar
 sudo chmod 544 /usr/bin/UserWebApp-0.0.1-SNAPSHOT.jar
 
+##Moving the service file to systemd location
 echo "Enabling Application as a Linux systemd service for auto-start on reboot"
 sudo mv /tmp/userWebApp.service /etc/systemd/system/userWebApp.service
 sudo systemctl daemon-reload
 sudo systemctl enable userWebApp.service
-sudo systemctl start userWebApp.service
+#sudo systemctl start userWebApp.service
 
 
