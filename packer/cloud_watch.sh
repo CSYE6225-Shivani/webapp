11d222b0c69a4a7b15bcc47410828461195b4daf
Shivani31996 <shivani.chavan96@gmail.com>
2023-03-20 20:46:06 -0400

    Testing

--- packer/cloud_watch.sh
+++ packer/cloud_watch.sh
@@ -2,3 +2,12 @@
 
 ########Installing CloudWatch###########
 sudo yum install amazon-cloudwatch-agent -y
+
+########Transferring Jar File & changing permissions to read and execute#########
+echo "Transferring Jar File & changing permissions to read and execute"
+sudo mv /tmp/amazon-cloudwatch-agent.json /usr/bin/
+sudo chmod 544 /usr/bin/amazon-cloudwatch-agent.json
+
+
+######Configuring AWS CloudWatch#########
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:/usr/bin/amazon-cloudwatch-agent.json
